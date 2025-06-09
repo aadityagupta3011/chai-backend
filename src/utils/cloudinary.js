c14d7aabb6b56f4192c2599b31352673f25af00a
takmonBoost <takmonboostgithub@gmail.com>
2025-06-09 21:10:22 +0530

    fix all register controler bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,20 +2,21 @@ import {v2 as cloudinary } from "cloudinary"
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name:'name',
-  api_key:'key',
-  api_secret:'secret',  
+  cloud_name:'dvxftpcom',
+  api_key:'428731746282345',
+  api_secret:'nX3-TDiGy_0gDeiPI7EcpqJwa3I',  
 })
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null
     //upload the file on cloudinary
-    const response = await cloudinary.uploader.upload.upload(localFilePath,{
+    const response = await cloudinary.uploader.upload(localFilePath,{
       resource_type : "auto",
     })
     //fu=ile has been uploaded successfully
-    console.log("File is uploaded on cloudinary",response.url);
+    // console.log("File is uploaded on cloudinary",response.url);
+    fs.unlinkSync(localFilePath)
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath) // remove the locally saved temp. file as the uploaded operation failed
